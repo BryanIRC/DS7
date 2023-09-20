0826690cbb7f0d6f413abb2d2779d363b3055887
BryanIRC <brios.16@gmail.com>
2023-09-20 16:17:13 -0500

    Igualdades

---
+++ lab2/lab26.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>laboratorio 2.6</title>
+</head>
+
+<body>
+    <?php
+    $a = 5;
+    $b = "5";
+    echo ($a == 5) ? "verdadero" : "falso", "<br>";
+    echo ($a === 5) ? "verdadero" : "falso", "<br>";
+    echo ($b == 5) ? "verdadero" : "falso", "<br>";
+    echo ($b === 5) ? "verdadero" : "falso", "<br>";
+    ?>
+</body>
+
+</html>
