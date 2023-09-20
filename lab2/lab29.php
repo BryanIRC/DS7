d872b02c52b32c9213a9178ddbe2cf0cc9f74762
BryanIRC <brios.16@gmail.com>
2023-09-20 16:57:35 -0500

    Ciclos con for

---
+++ lab2/lab29.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>laboratorio 2.9</title>
+</head>
+
+<body>
+    <?php
+    for ($i = 0; $i < 10; $i++) {
+        echo "El valor de i es: " . $i, "<br>";
+    }
+    ?>
+</body>
+
+</html>
